1039769410748e8476e7716586ca1349a7ecb630
Felipe Goncalves <felipe.cleia05@gmail.com>
2024-07-25 20:30:28 -0300

    adicionado fundo e botão claro/escuro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const mode = document.getElementById('mode_icon');
 const form = document.getElementById('login_form');
+const container = document.getElementById('container');
+const button = document.getElementById('login_button');
 
 mode.addEventListener('click', () => {
   if(mode.classList.contains('fa-moon')) {
@@ -7,10 +9,15 @@ mode.addEventListener('click', () => {
     mode.classList.add('fa-sun')
 
     form.classList.add('dark')
+    container.classList.add('container-dark')
+    button.classList.add('dark-button')
 
     return;
   }
     mode.classList.remove('fa-sun')
     mode.classList.add('fa-moon')
+    
     form.classList.remove('dark')
+    container.classList.remove('container-dark')
+    button.classList.remove('dark-button')
 });
